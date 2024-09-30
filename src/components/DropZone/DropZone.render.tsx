0ce6a5d97acb3b4025a9cf8c8ac86ea99b637130
Ayoub METWALLI <ayoub.metwalli@4d.com>
2024-09-30 10:53:16 +0100

    add css classes

--- src/components/DropZone/DropZone.render.tsx
+++ src/components/DropZone/DropZone.render.tsx
@@ -17,6 +17,12 @@ const DropZone: FC<IDropZoneProps> = ({ style, className, classNames = [] }) => 
   const handleFileSelection = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       const filesArray = Array.from(e.target.files);
+
+      // Filter for accepted file types and sizes (e.g., max 5MB)
+      /*const acceptedFiles = filesArray.filter(
+        (file) => file.type === 'image/png' || file.type === 'image/jpeg' // Example: Only accept PNG and JPEG
+      ).filter(file => file.size <= 5 * 1024 * 1024); // Example: Max size 5MB*/
+
       setFiles((prevFiles) => {
         const newFiles = filesArray.filter(
           (newFile) =>
@@ -36,6 +42,12 @@ const DropZone: FC<IDropZoneProps> = ({ style, className, classNames = [] }) => 
 
     if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
       const filesArray = Array.from(e.dataTransfer.files);
+
+      // Filter for accepted file types and sizes (e.g., max 5MB)
+      /*const acceptedFiles = filesArray.filter(
+        (file) => file.type === 'image/png' || file.type === 'image/jpeg' // Example: Only accept PNG and JPEG
+      ).filter(file => file.size <= 5 * 1024 * 1024); // Example: Max size 5MB*/
+
       setFiles((prevFiles) => {
         const newFiles = filesArray.filter(
           (newFile) =>
@@ -61,7 +73,12 @@ const DropZone: FC<IDropZoneProps> = ({ style, className, classNames = [] }) => 
 
   const handleDragLeave = (e: DragEvent<HTMLDivElement>) => {
     e.preventDefault();
-    setDragging(false);
+    const relatedTarget = e.relatedTarget as HTMLElement;
+    const isInside = e.currentTarget.contains(relatedTarget);
+
+    if (!isInside) {
+      setDragging(false);
+    }
   };
 
   /*useEffect(() => {
@@ -83,24 +100,31 @@ const DropZone: FC<IDropZoneProps> = ({ style, className, classNames = [] }) => 
 
   const handleRemoveFile = (event: any, file: File) => {
     event.stopPropagation();
-    console.log('handleRemoveFile', files);
     setFiles((prev) => prev.filter((f) => f !== file));
   };
 
   return (
-    <div ref={connect} style={style} className={cn(className, classNames)} onClick={handleClick}>
-      <div
-        onDrop={handleDrop}
-        onDragOver={handleDragOver}
-        onDragEnter={handleDragEnter}
-        onDragLeave={handleDragLeave}
-      >
-        {dragging ? (
-          <p>Drop files here...</p>
-        ) : (
-          <p>Drag & drop files here, or click to select files</p>
-        )}
-      </div>
+    <div
+      ref={connect}
+      style={{
+        ...style,
+        borderColor: dragging ? 'purple' : style?.borderColor,
+        color: dragging ? 'purple' : style?.color,
+      }}
+      className={cn(className, classNames, {
+        dragging,
+      })}
+      onClick={handleClick}
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+    >
+      {dragging ? (
+        <p>Drop files here...</p>
+      ) : (
+        <p>Drag & drop files here, or click to select files</p>
+      )}
       <input
         ref={inputRef}
         type="file"
@@ -108,21 +132,26 @@ const DropZone: FC<IDropZoneProps> = ({ style, className, classNames = [] }) => 
         style={{ display: 'none' }}
         onChange={handleFileSelection}
       />
-      {files.length > 0 && (
-        <div>
-          <h3>Selected Files:</h3>
-          <ul>
-            {files.map((file, index) => (
-              <li key={index}>
-                {file.name}{' '}
-                <span className="text-red-500" onClick={(e) => handleRemoveFile(e, file)}>
-                  X
-                </span>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <div className="selected-files text-left">
+        {files.length > 0 && (
+          <>
+            <h3 className="selected-files-title text-lg">Selected Files:</h3>
+            <ul className="selected-file">
+              {files.map((file, index) => (
+                <li className="selected-file-title" key={index}>
+                  {file.name}{' '}
+                  <span
+                    className="selected-file-icon text-red-500"
+                    onClick={(e) => handleRemoveFile(e, file)}
+                  >
+                    X
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </div>
   );
 };
